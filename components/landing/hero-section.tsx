1972686e2e830d4c2178d6ace7759da192c2c1f5
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-12-15 10:05:10 +0530

    feat: Implemented Google Oauth + Neondb connection and working

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -46,42 +46,6 @@ export default function HeroSection() {
         </div>
         <section>
           <div className="relative pt-24 md:pt-36">
-            <AnimatedGroup
-              variants={{
-                container: {
-                  visible: {
-                    transition: {
-                      delayChildren: 1,
-                    },
-                  },
-                },
-                item: {
-                  hidden: {
-                    opacity: 0,
-                    y: 20,
-                  },
-                  visible: {
-                    opacity: 1,
-                    y: 0,
-                    transition: {
-                      type: "spring",
-                      bounce: 0.3,
-                      duration: 2,
-                    },
-                  },
-                },
-              }}
-              className="mask-b-from-35% mask-b-to-90% absolute inset-0 top-56 -z-20 lg:top-32"
-            >
-              <Image
-                src="https://ik.imagekit.io/lrigu76hy/tailark/night-background.jpg?updatedAt=1745733451120"
-                alt="background"
-                className="hidden size-full dark:block"
-                width="3276"
-                height="4095"
-              />
-            </AnimatedGroup>
-
             <div
               aria-hidden
               className="absolute inset-0 -z-10 size-full [background:radial-gradient(125%_125%_at_50%_100%,transparent_0%,var(--color-background)_75%)]"
@@ -183,7 +147,7 @@ export default function HeroSection() {
                   <Button
                     asChild
                     size="lg"
-                    variant="ghost"
+                    variant="outline"
                     className={`h-10.5 rounded-xl px-5 cursor-pointer transition-opacity duration-300 ${
                       isSignedIn
                         ? "opacity-100"
