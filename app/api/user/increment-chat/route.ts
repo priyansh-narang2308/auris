225a91baaab30faecda6e6cadfaf61e6e02e2421
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-12-18 19:08:40 +0530

    feat: Incrementing the user chat route in progress

---
+++ app/api/user/increment-chat/route.ts
@@ -0,0 +1,51 @@
+import { prisma } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return NextResponse.json(
+        { error: "User not authenticated. Please login to continue" },
+        { status: 401 }
+      );
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        clerkId: userId,
+      },
+      select: {
+        id: true,
+        currentPlan: true,
+        subscriptionStatus: true,
+        chatMessagesToday: true,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const chattingChecking=await canUserChat(user.id)
+
+    if (!chattingChecking.allowed) {
+            return NextResponse.json({
+                error: chattingChecking.reason,
+                upgradeRequired: true
+            }, { status: 403 })
+        }
+
+       await incrementChatUsage(user.id)
+
+        return NextResponse.json({ success: true })
+
+
+
+  } catch (error) {
+            return NextResponse.json({ error: 'Failed to increment chat of user.' }, { status: 500 })
+
+  }
+}
