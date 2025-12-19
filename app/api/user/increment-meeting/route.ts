9a366aeb71c6ee95606c6c64af00f6f95a42e2fe
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-12-19 18:31:05 +0530

    feat: Increment meeting usage api route implemented

---
+++ app/api/user/increment-meeting/route.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { prisma } from "@/lib/db";
+import { incrementMeetingUsage } from "@/lib/usage";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return NextResponse.json(
+        { error: "User not authenticated" },
+        { status: 401 }
+      );
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        clerkId: userId,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    await incrementMeetingUsage(user.id);
+
+    return NextResponse.json({
+      success: true,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to increment meeting usage of the user." },
+      { status: 500 }
+    );
+  }
+}
