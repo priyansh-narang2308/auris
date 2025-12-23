3a1ca26c457f3b69c02caf83cb98ef0e12290608
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-12-23 09:42:05 +0530

    bug: resoved dependency bug in the code

--- components/ui/animated-tooltip.tsx
+++ components/ui/animated-tooltip.tsx
@@ -77,8 +77,8 @@ export const AnimatedTooltip = ({
                 }}
                 className="absolute -top-16 left-1/2 z-50 flex -translate-x-1/2 flex-col items-center justify-center rounded-md bg-black px-4 py-2 text-xs shadow-xl"
               >
-                <div className="absolute inset-x-10 -bottom-px z-30 h-px w-[20%] bg-gradient-to-r from-transparent via-orange-500 to-transparent" />
-                <div className="absolute -bottom-px left-10 z-30 h-px w-[40%] bg-gradient-to-r from-transparent via-yellow-500 to-transparent" />
+                <div className="absolute inset-x-10 -bottom-px z-30 h-px w-[20%] bg-linear-to-r from-transparent via-orange-500 to-transparent" />
+                <div className="absolute -bottom-px left-10 z-30 h-px w-[40%] bg-linear-to-r from-transparent via-yellow-500 to-transparent" />
                 <div className="relative z-30 text-base font-bold text-white">
                   {item.name}
                 </div>
@@ -88,14 +88,15 @@ export const AnimatedTooltip = ({
           </AnimatePresence>
           <div
             onMouseMove={handleMouseMove}
-            className="relative !m-0 h-8 w-8 rounded-full border-2 border-background
-             bg-gradient-to-br from-orange-500 to-orange-600
+            className="relative m-0! h-8 w-8 rounded-full border-2 border-background
+             bg-linear-to-br from-orange-500 to-orange-600
              flex items-center justify-center
              text-[10px] font-semibold text-white
              shadow-sm transition duration-300
              group-hover:z-30 group-hover:scale-110"
           >
             {item.image ? (
+              // eslint-disable-next-line @next/next/no-img-element
               <img
                 src={item.image}
                 alt={item.name}
