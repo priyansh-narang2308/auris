d61e7f40c920ddd75920dd65f3213d8ef8a0cf6f
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-12-29 00:55:31 +0530

    feat: Jira: The jira api getting all projects by ID added

--- lib/integrations/jira/jira.ts
+++ lib/integrations/jira/jira.ts
@@ -1,3 +1,23 @@
-export class JiraAPI{
-    
+export class JiraAPI {
+  private baseUrl = "https://api.atlassian.com/ex/jira";
+
+  async getProjectbyId(token: string, cloudId: string, projectId: string) {
+    const response = await fetch(
+      `${this.baseUrl}/${cloudId}/rest/api/3/project/${projectId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.error("Jira project error: ", response.status, errorText);
+      throw new Error(`Failed to fetch project ${response.status}`);
+    }
+
+    return response.json();
+  }
 }
