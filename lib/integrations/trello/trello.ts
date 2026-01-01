1d7a9baa1ba65c2fc105ef5afed091c363a94f85
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2026-01-01 23:52:38 +0530

    feat: Trello API integrated with its API url from document

---
+++ lib/integrations/trello/trello.ts
@@ -0,0 +1,69 @@
+import { ActionItemData } from "../types";
+
+export class TrelloAPI {
+  private apiKey = process.env.TRELLO_API_KEY!;
+
+  private baseUrl = "https://api.trello.com/1";
+
+  async getBoards(token: string) {
+    const response = await fetch(
+      `${this.baseUrl}/members/me/boards?key=${this.apiKey}&token=${token}`
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        "Failed to fetch boards via Trello. Please try again later."
+      );
+    }
+
+    return response.json();
+  }
+
+  async createBoard(token: string, name: string) {
+    const response = await fetch(
+      `${this.baseUrl}/boards?key=${
+        this.apiKey
+      }&token=${token}&name=${encodeURIComponent(name)}&defaultLists=true`,
+      { method: "POST" }
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        "Failed to create boards in Trello. Please try again later."
+      );
+    }
+
+    return response.json();
+  }
+
+  async getBoardLists(token: string, boardId: string) {
+    const response = await fetch(
+      `${this.baseUrl}/boards/${boardId}/lists?key=${this.apiKey}&token=${token}`
+    );
+
+    if (!response.ok) {
+      throw new Error("failed to fetch lists");
+    }
+
+    return response.json();
+  }
+
+  async createCard(token: string, listId: string, data: ActionItemData) {
+    const response = await fetch(
+      `${this.baseUrl}/cards?key=${
+        this.apiKey
+      }&token=${token}&idList=${listId}&name=${encodeURIComponent(
+        data.title
+      )}&desc=${encodeURIComponent(data.description || "")}`,
+      {
+        method: "POST",
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("failed to create card");
+    }
+
+    return response.json();
+  }
+}
