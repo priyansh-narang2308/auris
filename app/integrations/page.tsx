2f943bb206d6b975e15789a78780c4a76f7d22cc
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2026-01-02 19:40:20 +0530

    feat: UI: The selecting dropdown for integration added

--- app/integrations/page.tsx
+++ app/integrations/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+
 
 const IntegrationPage = () => {
-  return (
-    <div>IntegrationPage</div>
-  )
-}
+  return <div></div>;
+};
 
-export default IntegrationPage
+export default IntegrationPage;
